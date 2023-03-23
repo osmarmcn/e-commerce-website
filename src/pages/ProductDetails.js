20826cf89fb26654689155e2243f4ca14f5ccdcd
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-03-23 18:19:04 -0300

    ajuste

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -12,18 +12,25 @@ const ProductProvider = ({children}) => {
   //products state
   const [products, setProducts] = useState([])
 
-  //fetch products
-  const fetchProducts = async () =>{
-    const response = await fetch('https://fakestoreapi.com/products')
-    //.then(res => res.json())
-    //.then(json => console.log(json))
 
-    const data = await response.json()
-    console.log(data)
-  }
+ useEffect(()=>{
+    //fetch products
+    const fetchProducts = async () =>{
+        const response = await fetch('https://fakestoreapi.com/products')
+        //.then(res => res.json())
+        //.then(json => console.log(json))
+
+        const data = await response.json()
+        //console.log(data)
+        setProducts(data)
+    }
+
+    fetchProducts()
+ }, []) 
+  
 
 
-  return <ProductContext.Provider>{children}</ProductContext.Provider>
+  return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>
 };
 
 export default ProductProvider;
