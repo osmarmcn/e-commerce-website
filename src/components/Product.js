4fffd8e56e95570fcaf092cfa879fd593c206c4b
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-03-25 18:58:55 -0300

    ajuste

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,16 +17,20 @@ const Product = ({product}) => {
           <div className='w-[200px] mx-auto flex justify-center items-center'>
             <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt=''/>
           </div>
-          <div className='absolute top-0 right-0 bg-red-500 p-2'>
+          <div className='absolute top-6 right-11 group-hover:right-5 p-2 flex flex-col items-center justify-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300'>
             <button>
-              <div className='flex justify-center items-center text-white w-12 h-12'>
+              <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
                 <BsPlus className='text-3xl'/>
               </div>
             </button>
+            <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'>
+              <BsEyeFill/>
+            </Link>
           </div>
 
         </div>
       </div>
+      {/*category, title e price */}
       <div>2</div>
     </div>
   )
