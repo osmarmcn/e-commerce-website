2ab444a74f385e4e8dce70a7d2bacfc84c33a406
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-03-28 16:12:05 -0300

    ajuste

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import {IoMdClose} from 'react-icons/io'
+import {IoMdAdd, IoMdClose, IoMdRemove} from 'react-icons/io'
 
 const CartItem = ({item}) => {
   const {id, title, image, price, amount} = item
 
   return(
-    <div className='felx'>
+    <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
       <div className='w-full min-h-[150px] flex items-center gap-x-4'>
         {/*image */}
         <Link to={`/product/${id}`}>
@@ -26,13 +26,25 @@ const CartItem = ({item}) => {
                   <IoMdClose className='text-gray-500 hover:text-red-500 transition'/>
               </div>
             </div>
-             <div>
+             <div className='flex gap-x-2 h-[3px] text-sm'>
                 {/*quantity */}
-                <div>quantity</div>
+                <div className='flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium'>
+                   {/*minus icon*/}
+                   <div className='flex-1'>
+                    <IoMdRemove/>
+                   </div>
+                   
+                   {/*amount*/}
+                   <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+                   {/*plus icon*/}
+                   <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+                    <IoMdAdd/>
+                   </div>
+                </div>
                 {/*item price */}
-                <div>item price</div>
+                <div className='flex-1 flex items-center justify-around'>$ {price}</div>
                 {/*final price */}
-                <div>final price</div>
+                <div className='flex-1 flex justify-end items-center text-priamry font-medium'>{`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
              </div>
           </div>
         </div>
