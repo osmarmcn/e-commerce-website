3ddb46268eb88c92e90a22be3f998ae5fd78c05d
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-03-31 15:52:34 -0300

    ajuste

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -34,13 +34,20 @@ const {cart, clearCart, total} = useContext(CartContext)
           <div className=' flex w-full justify-between items-center'>
             {/* total*/}
             <div className='uppercase font-semibold'>
-              <span className='mr-2'>Total:</span>${total }
+              <span className='mr-2'>Total:</span>${parseFloat(total).toFixed(2) }
             </div>
             {/*clear cart icon */}
             <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 flex justify-center items-center text-xl'>
               <FiTrash2/>
             </div>
           </div>
+          <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-priamry w-full font-medium'>
+            View cart
+          </Link>
+          <Link to='/' className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium' >
+            Checkout
+          </Link>
+
         </div>
      </div>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import Product from '../components/Product';
 import { ProductContext } from '../contexts/ProductContext';
+import Hero from '../components/Hero';
 
 //import product context
 
@@ -19,6 +20,7 @@ const Home = () => {
 
  return(
   <div>
+    <Hero/>
     <section className='py-16'>
       <div className='container mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
