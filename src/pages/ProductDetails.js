da445fb278817e439820178d0f344965df197b5e
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-03-31 18:00:57 -0300

    ajuste

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,9 +1,46 @@
-import Reac, {useContext} from 'react';
+import React, {useContext} from 'react';
 
+import { useParams } from 'react-router-dom';
 
+import { CartContext } from '../contexts/CartContext';
+import { ProductContext } from '../contexts/ProductContext';
 
 const ProductDetails = () => {
-  return <div>Product Details Page</div>;
+
+  const {id} = useParams()
+  const {products} = useContext(ProductContext)
+  const {addToCart} = useContext(CartContext)
+
+  const product = products.find((item) =>{
+    return item.id === parseInt(id) 
+  })
+
+  if(!product){
+    return <section className='h-screen flex justify-center items-center'>Loading...</section>
+  }
+
+
+  const {title, price, description, image} = product
+
+  return (
+    <section className='pt-32 pb-12 lg:py-32 h-screen flex items-center'>
+      <div className='container mx-auto'>
+        
+        <div className='flex flex-col lg:flex-row'>
+          {/*image */}
+          <div>
+            <img src={image} alt=''/>
+
+          </div>
+            {/*text */}
+           <div>text</div>
+        </div>
+        
+       
+
+      </div>
+    </section>
+  ) 
 };
 
 export default ProductDetails;
