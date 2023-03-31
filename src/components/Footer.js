c59b0c09e4fe22092f78721edb111ffaea492c0f
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-03-31 16:12:16 -0300

    criando a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return( 
+  <footer className='bg-priamry py-12'>
+    <div className='container mx-auto'>
+      <p className='text-white text-center'>Copyright &copy; Ecommerce Shop 2023. All rights reservd.</p>
+    </div>
+
+  </footer>
+  )
 };
 
 export default Footer;
