b5d7153664d500914848aeda889407085759a8b7
osmar mendes <99884631+osmarmcn@users.noreply.github.com>
2023-04-04 15:40:30 -0300

    ajuste

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -28,12 +28,17 @@ const ProductDetails = () => {
         
         <div className='flex flex-col lg:flex-row'>
           {/*image */}
-          <div>
-            <img src={image} alt=''/>
+          <div className='flex flex-1  justify-center items-center mb-8 lg:mb-0'>
+            <img className='max-w-[200px] lg:max-w-sm' src={image} alt=''/>
 
           </div>
             {/*text */}
-           <div>text</div>
+           <div className='flex-1 text-center lg:text-left'>
+            <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>{title}</h1>
+            <div className='text-xl text-red-500 font-medium mb-6'>$ {price}</div>
+            <p className='mb-8'>{description}</p>
+            <button onClick={()=> addToCart(product, product.id)} className='bg-primary py-4 px-8 text-white '>add to cart</button>
+           </div>
         </div>
         
        
